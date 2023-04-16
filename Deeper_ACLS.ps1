0bbee58e5be90da1e5ac78ae7ec2d3321eae40e0
f0ns1 <61078210+f0ns1@users.noreply.github.com>
2023-04-16 18:08:15 +0200

    Update Deeper_ACLS.ps1

--- Deeper_ACLS.ps1
+++ Deeper_ACLS.ps1
@@ -26,7 +26,16 @@ function Get-ADPrincipalGroupMembershipRecursive ($SamAccountName)
 }
 # Deepr ACLs
 function deeper-acls($filter){
-            Get-DomainObjectAcl | select -expandProperty ObjectDN  | Get-Unique | % {$permissions=@(Get-Acl AD:\$_ | select -ExpandProperty Access | ?{$_.IdentityReference -like "*$filter*"} | Format-Table);if($permissions){Write-Host $_; Get-ADObject -SearchBase $_ | select name, objectclass ;$permissions}}
+   # if (-not ($filter -contains "Domain Users")){
+     #   if( -not ($filter -contains "Users" )){
+            Get-DomainObjectAcl | select -expandProperty ObjectDN  | Get-Unique | 
+            %{$permissions=@(Get-Acl AD:\$_ | 
+            select -ExpandProperty Access | 
+            ?{$_.IdentityReference -like "*$filter*"} | Format-Table);
+            if($permissions){Write-Host $_; Get-ADObject -SearchBase $_ | 
+            select name, objectclass ;$permissions}}
+     #   }
+    #}
 }
 
 
@@ -39,6 +48,9 @@ function deeper-acls($filter){
 Write-Host "Input users to check $users"
 foreach($user in $users.Split(" ")){
     Write-Host "User : $user"
-    Get-ADPrincipalGroupMembershipRecursive "$user" | %{ $filter=$_.Split("=")[1].Split(",")[0]; Write-Host "ACLS for membership $user - $_ - $filter"; deeper-acls $filter} 
+    Get-ADPrincipalGroupMembershipRecursive "$user" |
+     %{ $filter=$_.Split("=")[1].Split(",")[0]; 
+     Write-Host "ACLS for membership $user - $_ - $filter"; 
+     deeper-acls $filter} 
 
 }
